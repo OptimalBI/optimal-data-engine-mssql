3eb757f337b52d7f88a88dced24c883d6a049bab
Brian Bradley <brian.bradley@optimalbi.com>
2015-11-16 10:10:01 +1300

    Added an Order Clause to the process to enable reliable emptying of Config Tables.

--- dv_config/Stored Procedures/dv_global_config_update.sql
+++ dv_config/Stored Procedures/dv_global_config_update.sql
@@ -1,5 +1,5 @@
 ﻿
-Create PROCEDURE [dv_config].[dv_global_config_update]
+CREATE PROCEDURE [dv_config].[dv_global_config_update]
 (
      @vault_config_update_type				varchar(50)		= 'Set_Release'
 	,@vault_release_number					int				= 0
@@ -80,6 +80,7 @@ begin
 declare Config_Cursor cursor forward_only for 
 select  dv_schema_name, dv_table_name
 from [dv_release].[fn_config_table_list] ()
+order by dv_load_order desc
 
 open Config_Cursor
 fetch next from Config_Cursor into  @schema_name, @table_name				
