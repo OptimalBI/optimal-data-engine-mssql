f324aa910a9ea6eaa66dec96f89fed9204d3af33
Brian Bradley <brian.bradley@optimalbi.com>
2016-01-19 16:48:47 +1300

    Update the Release Table to 003

--- ODE_release/Tables/dv_release_001_001_001.sql
+++ ODE_release/Tables/dv_release_001_001_001.sql
@@ -1,11 +1,6 @@
-﻿CREATE TABLE [ODE_Release].[dv_release_001_001_001] (
-    [release_key]          INT                IDENTITY (1, 1) NOT NULL,
-    [release_applied_date] DATETIMEOFFSET (7) NULL,
-    PRIMARY KEY CLUSTERED ([release_key] ASC)
-);
+﻿
 
 
 GO
-CREATE NONCLUSTERED INDEX [PK_dv_release_001_001_001_Column]
-    ON [ODE_Release].[dv_release_001_001_001]([release_key] ASC);
+
 
---
+++ ODE_release/Tables/dv_release_001_001_003.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [ODE_Release].[dv_release_001_001_003] (
+    [release_key]          INT                IDENTITY (1, 1) NOT NULL,
+    [release_applied_date] DATETIMEOFFSET (7) NULL,
+    PRIMARY KEY CLUSTERED ([release_key] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [PK_dv_release_001_001_001_Column]
+    ON [ODE_Release].[dv_release_001_001_003]([release_key] ASC);
+
